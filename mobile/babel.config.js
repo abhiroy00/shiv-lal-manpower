9aa2f9fcf764d79f6a1177c78e1ea96c1a6bb6ef
Abhishek kumar <120657208+abhiroy00@users.noreply.github.com>
2026-06-25 21:22:26 +0530

    feat: mobile payslip, cleartext, eas config

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,8 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { unstable_transformProfile: "hermes-canary" }],
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
